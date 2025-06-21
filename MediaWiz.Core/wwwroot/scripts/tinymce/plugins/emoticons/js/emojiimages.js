6391ba3c3f23dad1ce621825fd44c3af674acb56
huwred <huw@forum.snitz.com>
2025-06-21 10:46:42 +0200

    Include tinymce javascript files

---
+++ MediaWiz.Core/wwwroot/scripts/tinymce/plugins/emoticons/js/emojiimages.js
@@ -0,0 +1,172 @@
+// Source: npm package: emojilib
+// Images provided by twemoji: https://github.com/twitter/twemoji
+window.tinymce.Resource.add("tinymce.plugins.emoticons", {
+  grinning: {
+    keywords: [ "face", "smile", "happy", "joy", ":D", "grin" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude00" src="icon_smile_big.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  smiley: {
+    keywords: [ "face", "happy", "joy", "haha", ":)", "smile", "funny" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude03" src="icon_smile.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  wink: {
+    keywords: [ "face", "happy", "mischievous", "secret", ";)", "smile", "eye" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude09" src="icon_smile_wink.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  blush: {
+    keywords: [ "face", "smile", "happy", "flushed", "crush", "embarrassed", "shy", "blush" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude0a" src="icon_smile_blush.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  stuck_out_tongue: {
+    keywords: [ "face", "prank", "childish", "playful", "mischievous", "smile", "tongue" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude1b" src="icon_smile_tongue.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  clown_face: {
+    keywords: [ "face" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83e\udd21" src="icon_smile_clown.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  dead: {
+    keywords: [ "face", "dead", "-_-", "meh", "deadpan" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude11" src="icon_smile_dead.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  unamused: {
+    keywords: [ "indifference", "bored", "straight face", "serious", "sarcasm", "unimpressed", "skeptical", "dubious", "side_eye" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude12" src="icon_smile_dissapprove.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  hand_over_mouth: {
+    keywords: [ "face", "whoops", "shock", "surprise" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83e\udd2d" src="icon_smile_shock.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  angry: {
+    keywords: [ "mad", "face", "annoyed", "frustrated" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude20" src="icon_smile_angry.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  very_angry: {
+    keywords: [ "mad", "face", "annoyed", "frustrated" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude20" src="icon_smile_angry2.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  confused: {
+    keywords: [ "face", "indifference", "huh", "weird", "hmmm", ":/" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude15" src="icon_smile_mean.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  approve: {
+    keywords: [ "face", "approve","^" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude15" src="icon_smile_approve.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  disapprove: {
+    keywords: [ "face", "disapprove","V" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude15" src="icon_smile_dissapprove.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  bigeyes: {
+    keywords: [ "eyes", "bigeyes", "surprise" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2639" src="icon_smile_bigeyes.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  blackeye: {
+    keywords: [ "blackeye", "punch", "eyes" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2639" src="icon_smile_blackeye.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  boggled: {
+    keywords: [ "boggled", "confused", "huh" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2639" src="icon_smile_boggled.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  grumpy: {
+    keywords: [ "grumpy", "sad", "upset" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2639" src="icon_smile_test.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  frowning_face: {
+    keywords: [ "face", "sad", "upset", "frown" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2639" src="icon_smile_sad.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  sleepy: {
+    keywords: [ "face", "tired", "rest", "nap" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\ude2a" src="icon_smile_sleepy.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  imp: {
+    keywords: [ "devil", "angry", "horns" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\udc7f" src="icon_smile_evil.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  "+1": {
+    keywords: [ "thumbsup", "yes", "awesome", "good", "agree", "accept", "cool", "hand", "like" ],
+    char: '<img data-emoticon="true" style="width:1.3em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\udc4d" src="icon_thumb.gif"/>',
+    fitzpatrick_scale: true,
+    category: "people"
+  },
+  kiss: {
+    keywords: [ "mouth", "kiss" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\udc44" src="icon_smile_kisses.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  dark_sunglasses: {
+    keywords: [ "face", "cool", "accessories" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\udd76" src="icon_smile_cool.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  cyclops: {
+    keywords: [ "face", "oneeye", "eye" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83d\udd76" src="icon_smile_cyclops.gif"/>',
+    fitzpatrick_scale: false,
+    category: "people"
+  },
+  "8ball": {
+    keywords: [ "eightball", "hobby", "game", "luck", "magic" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\ud83c\udfb1" src="icon_smile_8ball.gif"/>',
+    fitzpatrick_scale: false,
+    category: "activity"
+  },
+  heart: {
+    keywords: [ "love", "like", "valentines" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2764\ufe0f" src="heart_icon.png"/>',
+    fitzpatrick_scale: false,
+    category: "symbols"
+  },
+  question: {
+    keywords: [ "?", "question", "What" ],
+    char: '<img data-emoticon="true" style="width:1em;height:1em;margin:0 .05em 0 .1em;vertical-align:-.1em" draggable="false" alt="\u2764\ufe0f" src="icon_smile_question.gif"/>',
+    fitzpatrick_scale: false,
+    category: "symbols"
+  }
+});
