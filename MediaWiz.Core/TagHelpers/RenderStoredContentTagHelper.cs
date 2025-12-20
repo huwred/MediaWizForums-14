01c19e1af2c50e73637af274c37c2506115ebff2
huwred <huw@forum.snitz.com>
2025-12-20 02:15:47 +0100

    new tagHelpers to store and render style/script tags in parent Views

---
+++ MediaWiz.Core/TagHelpers/RenderStoredContentTagHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MediaWiz.Forums.TagHelpers
+{
+    /// <summary>
+    /// A <see cref="TagHelper"/> that renders HTML content stored in the <see cref="ITempDataDictionary"/>  under a
+    /// specific key or all stored content if no key is provided.
+    /// </summary>
+    /// <remarks>This tag helper retrieves HTML content stored in the <see cref="ITempDataDictionary"/> using
+    /// a predefined  storage key. The content can be filtered by specifying the <c>asp-key</c> attribute. If the
+    /// attribute is  not provided, all stored content will be rendered. The rendered content is appended to the
+    /// output.</remarks>
+    [HtmlTargetElement("renderstoredcontent")]
+    public class RenderStoredContentTagHelper : TagHelper
+    {
+        private const string KeyAttributeName = "asp-key";
+        private const string _storageKey = "storecontent";
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext? ViewContext { get; set; }
+
+        [HtmlAttributeName(KeyAttributeName)]
+        public required string Key { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = String.Empty;
+
+            var storageProvider = ViewContext!.TempData;
+            Dictionary<string, List<HtmlString>>? storage;
+
+            if (!storageProvider.ContainsKey(_storageKey) || !(storageProvider[_storageKey] is Dictionary<string, List<HtmlString>>))
+            {
+                return;
+            }
+
+            storage = storageProvider[_storageKey] as Dictionary<string, List<HtmlString>>;
+            string html = "";
+
+            if (String.IsNullOrEmpty(Key))
+            {
+                html = String.Join("", storage!.Values.SelectMany(x => x).ToList());
+            }
+            else
+            {
+                if (!storage!.ContainsKey(Key)) return;
+                html = String.Join("", storage[Key]);
+            }
+
+            TagBuilder tagBuilder = new TagBuilder("dummy");
+            tagBuilder.InnerHtml.AppendHtml(html) ;
+            output.Content.AppendHtml(html);
+        }
+    } 
+}
---
+++ MediaWiz.Core/TagHelpers/StoreContentTagHelper.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediaWiz.Forums.TagHelpers
+{ 
+    /// <summary>
+    /// A custom <see cref="TagHelper"/> that stores the inner content of the `<storecontent>` tag into a temporary storage,
+    /// allowing it to be retrieved and reused later in the same request.
+    /// </summary>
+    /// <remarks>The content is stored in the <see cref="ViewContext.TempData"/> dictionary under a predefined key. If
+    /// the <c>asp-key</c> attribute is specified, the content is stored in a sub-key corresponding to the provided value.
+    /// If no key is specified, the content is stored in a default list. This tag helper is useful for scenarios where
+    /// content needs to be dynamically aggregated or reused across different parts of a view.</remarks>
+    [HtmlTargetElement("storecontent")]
+    public class StoreContentTagHelper : TagHelper
+    {
+        private const string KeyAttributeName = "asp-key";
+        private const string _storageKey = "storecontent";
+        private const string _defaultListKey = "DefaultKey";
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext? ViewContext { get; set; }
+
+        [HtmlAttributeName(KeyAttributeName)]
+        public required string Key { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SuppressOutput();
+            TagHelperContent childContent = await output.GetChildContentAsync();
+
+            var storageProvider = ViewContext!.TempData;
+            Dictionary<string, List<HtmlString>>? storage;
+            List<HtmlString> defaultList;
+
+            if (!storageProvider.ContainsKey(_storageKey) || !(storageProvider[_storageKey] is Dictionary<string,List<HtmlString>>))
+            {
+                storage = new Dictionary<string, List<HtmlString>>();
+                storageProvider[_storageKey] = storage;
+                defaultList = new List<HtmlString>();
+                storage.Add(_defaultListKey, defaultList);
+            }
+            else
+            {
+                storage = ViewContext.TempData[_storageKey] as Dictionary<string, List<HtmlString>>;
+                if (storage!.ContainsKey(_defaultListKey))
+                {
+                    defaultList = storage[_defaultListKey];
+
+                }
+                else
+                {
+                    defaultList = new List<HtmlString>();
+                    storage.Add(_defaultListKey, defaultList);
+                }
+            }
+
+            if (string.IsNullOrEmpty(Key))
+            {
+                defaultList.Add(new HtmlString(childContent.GetContent()));
+            }
+            else
+            {
+                if(storage.ContainsKey(Key))
+                {
+                    storage[Key].Add(new HtmlString(childContent.GetContent()));
+                }
+                else
+                {
+                    storage.Add(Key, new List<HtmlString>() { new HtmlString(childContent.GetContent()) });
+                }
+            }
+        }
+    } 
+    }
